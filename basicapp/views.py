fe859bf0dbf3d745fae7b05e93424f088a32e080
saqazhar <s.a.q.professional@gmail.com>
2018-12-28 05:55:09 +0530

    Form page is not displlaying the input

--- basicapp/views.py
+++ basicapp/views.py
@@ -6,5 +6,16 @@ def index(request):
     return render(request, 'index.html')
 
 def form_name(request):
-    ffc = froms.FormName()
-    return render(request, 'form_page.html', {'form': ffc})
+    form = froms.FormName()
+
+    if request.method == 'post':
+        form = forms.FormName(request.post)
+
+        if form.is_valid:
+            print('Validation is success you can move forward')
+
+            print('UserName:'+form.cleaned_data['User_Name'])
+            print('Email:' + form.cleaned_data['Email'])
+            print('Text:' + form.cleaned_data['Text'])
+
+    return render(request, 'form_page.html', {'form': form})
